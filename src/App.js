2765847328fdc8b6267d767c4b7c87732a64328d
ksdevinney <katie@devinney.org>
2022-05-28 20:57:09 -0500

    all images working, except cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// https://www.youtube.com/watch?v=I2UBjN5ER4s
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';

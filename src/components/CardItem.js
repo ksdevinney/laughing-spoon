51b39c7f9131a8d5b23bd38bd7de3ee3cb788f35
ksdevinney <katie@devinney.org>
2022-05-28 21:53:24 -0500

    fixed sizing on cards, moved images back to public

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,7 +7,7 @@ function CardItem(props) {
     <div>
         <li className='cards__item'>
             <Link className='cards__item__link' to={props.path}>
-                <figure className='cards__item__pic__wrap' data-category={props.label}>
+                <figure className='cards__item__pic-wrap' data-category={props.label}>
                     <img 
                         src={props.src} 
                         alt='Travel' 
